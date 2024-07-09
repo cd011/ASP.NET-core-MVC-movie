91d32ea3f49e640ca4e7d84318a9909c36f499e4
cd011 <zerocj00@gmail.com>
2024-07-09 16:01:40 +0530

    first commit 16:01-09/07/2024

---
+++ MvcMovie/Controllers/HellowWorldController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers;
+
+public class HelloWorldController : Controller
+{
+    //
+    //GET: /HelloWorld/
+    //don't have to add Index as an action because it is called as default
+    public string Index()
+    {
+        return "This is my default action...";
+    }
+    //
+    //GET: /HelloWorld/Welcome/
+    //Requires using System.Text.Encodings.Web;
+    public string Welcome(string name, int ID = 1)
+    {
+        //return "This is the Welcome action method...";
+
+        //called by localhost:{PORT}/HelloWorld/Welcome?name=Rick&numtimes=4
+        //return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        
+        //called by localhost:{PORT}/HelloWorld/Welcome/3?name=Rick
+        //third URL segment is considered as the route parameter id
+        return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+    }
+}
